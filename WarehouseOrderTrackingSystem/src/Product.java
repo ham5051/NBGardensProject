e1a24eed75ceba8077111a54cec81d9780be7dab
unknown <jham@PC-CZC530033C.netbuilder.private>
2015-09-22 13:03:29 +0100

    Commiting WarehouseOrderTrackingSystem

--- WarehouseOrderTrackingSystem/src/Product.java
+++ WarehouseOrderTrackingSystem/src/Product.java
@@ -2,5 +2,7 @@
 public class Product {
 
 
+//class constructor
+	
 
 }
--- WarehouseOrderTrackingSystem/src/WarehouseOrderTrackingApplication.java
+++ WarehouseOrderTrackingSystem/src/WarehouseOrderTrackingApplication.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class WarehouseOrderTrackingApplication {
 
 	public static void main(String[]args) {
+		
+       
+		System.out.println("-------------------------------");
+		System.out.println("Warhouse Order Tracking System:");
+		System.out.println("-------------------------------");
+		System.out.println("Please Initiate to Start:");
+	 Scanner scan = new Scanner(System.in);
+
+		System.out.println(scan.nextLine());
 		}
 }
